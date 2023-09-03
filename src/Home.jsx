28e3a692d4022f30b6c4dc98be9b21f7d254a922
FishySenpai <akanomanbasit@gmail.com>
2023-09-03 13:08:28 +0500

    added dropdown, func for sending,changing voiceid

--- src/Home.jsx
+++ src/Home.jsx
@@ -15,23 +15,15 @@ const Home = () => {
   const [audioSources, setAudioSources] = useState({});
   const [loginStatus, setLoginStatus] = useState("");
   const [voices, setVoices] = useState([]);
-  const [pause, setPause] = useState(false);
+  const [selectedVoice, setSelectedVoice] = useState();
+  const [selectVoice, setSelectVoice] = useState(false);
   const audioRef = useRef(null);
 
   axios.defaults.withCredentials = true;
-  
 
-  const toggleAudio = () => {
-    if (pause) {
-      audioRef.current.pause();
-    } else {
-      audioRef.current.play();
-    }
-    setPause(!pause)
+  const dropDown = () => {
+    setSelectVoice(!selectVoice);
   };
-   const handleAudioEnded = () => {
-     setPause(false); // Set isPlaying to false when audio ends
-   };
 
   const checkLogin = async () => {
     try {
@@ -111,9 +103,10 @@ const Home = () => {
 
   const Fetch = async () => {
     console.log(description);
-    let id = 2;
+    let voiceId = selectedVoice?.voice_id;
+    let id = null;
     try {
-      const body = { description, id };
+      const body = { description, id,voiceId };
       const response = await fetch("http://localhost:5000/text", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -124,6 +117,7 @@ const Home = () => {
       console.log(err);
     }
   };
+
   const Voices = async () => {
     try {
       const response = await fetch("https://api.elevenlabs.io/v1/voices", {
@@ -141,6 +135,8 @@ const Home = () => {
       const data = await response.json();
       console.log(data);
       setVoices(data.voices); // Assuming the response is JSON data
+      setSelectedVoice(data.voices[0]);
+      
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -151,39 +147,67 @@ const Home = () => {
         <button onClick={Voices}>Get voices</button>
       </div>
       {console.log(voices)}
-      <div className="px width">
-        {voices?.slice(0, 1).map((voice) => (
-          <div className="flex flex-row">
-            
-            <div className="pr">
-              <AudioControl audioSrc={voice.preview_url} />
-            </div>
-            <div className="pr">{voice.name}</div>
+      <div className="relative">
+        <div className="px width">
+          {console.log(selectedVoice)}
+          {selectedVoice ? (
             <div className="flex flex-row">
-              <div className="pr mr border">{voice.labels.accent}</div>
-              <div className="pr mr border">{voice.labels.age}</div>
-              <div className="pr mr border">{voice.labels.gender}</div>
-              <div className="pr mr border">{voice.labels.description}</div>
+              <div className="pr">
+                <AudioControl audioSrc={selectedVoice.preview_url} />
+              </div>
+              <div className="pr">{selectedVoice.name}</div>
+              <div className="flex flex-row">
+                <div className="pr mr border">
+                  {selectedVoice.labels.accent}
+                </div>
+                <div className="pr mr border">{selectedVoice.labels.age}</div>
+                <div className="pr mr border">
+                  {selectedVoice.labels.gender}
+                </div>
+                <div className="pr mr border">
+                  {selectedVoice.labels.description}
+                </div>
+              </div>
+              <button className="btn-clr" onClick={dropDown}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="30px"
+                  viewBox="0 0 320 512"
+                >
+                  <path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z" />
+                </svg>
+              </button>
             </div>
-          </div>
-        ))}
-      </div>
-      <div className="px width">
-        {voices?.slice(0, 10).map((voice) => (
-          <div className="flex flex-row">
-            
-           <div>
-              <AudioControl audioSrc={voice.preview_url} />
+          ) : null}
+
+          {selectVoice ? (
+            <div className="position-dropdown height">
+              {voices?.slice(0, 30).map((voice) => (
+                <button
+                  onClick={() => {
+                    setSelectedVoice(voice);
+                  }}
+                  className="btn-clr align-left"
+                >
+                  <div className="flex flex-row ">
+                    <div className="pr">
+                      <AudioControl audioSrc={voice.preview_url} />
+                    </div>
+                    <div className="pr clr">{voice.name}</div>
+                    <div className="flex flex-row">
+                      <div className="pr mr border">{voice.labels.accent}</div>
+                      <div className="pr mr border">{voice.labels.age}</div>
+                      <div className="pr mr border">{voice.labels.gender}</div>
+                      <div className="pr mr border">
+                        {voice.labels.description}
+                      </div>
+                    </div>
+                  </div>
+                </button>
+              ))}
             </div>
-            <div className="pr">{voice.name}</div>
-            <div className="flex flex-row">
-              <div className="pr mr border">{voice.labels.accent}</div>
-              <div className="pr mr border">{voice.labels.age}</div>
-              <div className="pr mr border">{voice.labels.gender}</div>
-              <div className="pr mr border">{voice.labels.description}</div>
-            </div>
-          </div>
-        ))}
+          ) : null}
+        </div>
       </div>
       <div>{loginStatus}</div>
       <div>
